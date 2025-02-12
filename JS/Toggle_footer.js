e0a319b4f50e53271dbb3d062ae70a4e6a8340d8
DaveX9 <sahadevthapa645462@gmail.com>
2025-02-12 11:51:09 +0700

    yes

--- JS/Toggle_footer.js
+++ JS/Toggle_footer.js
@@ -1,13 +1,13 @@
 
-function toggleMenu(menuId) {
-    let menu = document.getElementById(menuId);
-    let header = menu.previousElementSibling; // Get the h3
+// function toggleMenu(menuId) {
+//     let menu = document.getElementById(menuId);
+//     let header = menu.previousElementSibling; // Get the h3
 
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-        header.classList.remove("active");
-    } else {
-        menu.style.display = "block";
-        header.classList.add("active");
-    }
-}
+//     if (menu.style.display === "block") {
+//         menu.style.display = "none";
+//         header.classList.remove("active");
+//     } else {
+//         menu.style.display = "block";
+//         header.classList.add("active");
+//     }
+// }
